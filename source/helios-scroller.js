4f5bc5261ba34d7144dc5ff7946512211a3ba3ab
dansundy <dansundy@gmail.com>
2014-02-14 10:49:48 -0500

    Add touch and key support.

--- source/helios-scroller.js
+++ source/helios-scroller.js
@@ -9,54 +9,108 @@
                      * Set all the initial scroll variables within
                      * the $scope of the scroller-master element.
                      */
+
+                    // The elements
+                    var el = $elem;
+                    var $el = angular.element($elem);
+
+                    // The heights
                     var windowHeight = window.innerHeight || document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
-                    var elemHeight = $elem[0].offsetHeight;
+                    var elemHeight = el[0].offsetHeight;
                     var scrollableHeight = elemHeight - windowHeight;
 
-                    $scope.tween = null;
+                    // The prefixes
+                    var transformPrefix;
+
+                    // The current info
+                    var data = {
+                        tween: null,
+                        direction: {
+                            last: 0,
+                            current: 0
+                        },
+                        progress: {
+                            last: {y:0},
+                            current: {y:0},
+                            rendered: 0
+                        }
+                    }
+
+                    // The outputs
+                    // $scope.tween = null;
                     $scope.scrollTotal = 0;
                     $scope.percentComplete = 0;
-                    $scope.direction = {
-                        last: 0,
-                        current: 0
-                    };
-                    $scope.progress = {
-                        last: {y: 0},
-                        current: {y: 0}
-                    };
+                    // $scope.direction = {
+                    //     last: 0,
+                    //     current: 0
+                    // };
+                    // $scope.progress = {
+                    //     last: {y: 0},
+                    //     current: {y: 0},
+                    //     rendered: 0
+                    // };
 
                     /**
                      * All the Utility functions that can be called within
                      * the directive at any time.
                      */
                     var Utils = {
+                        getPrefix: function(prefixes) {
+                            var tmp = document.createElement('div');
+                            var result = '';
+
+                            for (var i = 0; i < prefixes.length; ++i) {
+                                if (typeof tmp.style[prefixes[i]] != 'undefined') {
+                                    result = prefixes[i];
+                                    break;
+                                } else {
+                                    result = null;
+                                }
+                            }
+
+                            return result;
+                        },
+                        render: function(value) {
+                            //console.log(value - $scope.progress.rendered);
+                            if (transformPrefix) {
+                                // Browser supports transform.
+                                el[0].style[transformPrefix] = 'translateY(' + value + 'px)';
+                            } else {
+                                $el.css('top', value + 'px');
+                            }
+                            
+                            data.direction.last = data.direction.current;
+                            data.progress.rendered = value;
+                            Utils.calculatePercent(value);
+                        },
                         tween: {
-                            set: function(delta) {
-                                if ($scope.direction.last !== 0 && $scope.direction.last !== $scope.direction.current) {
+                            set: function(to, from, time) {
+                                if (data.direction.last !== 0 && data.direction.last !== data.direction.current) {
                                     // Terminating the current tween due to change in direction;
                                     Utils.tween.terminate();
                                 }
-                                if (!$scope.tween) {
-                                    $scope.tween = new TWEEN.Tween( $scope.progress.last ).to( $scope.progress.current, 1500 )
+                                if (!data.tween) {
+                                    var tm = time || 1500;
+                                    var t = to || data.progress.current;
+                                    var f = from || data.progress.last;
+
+                                    data.tween = new TWEEN.Tween(f).to(t, tm)
                                         .easing(TWEEN.Easing.Cubic.Out)
                                         .onUpdate( function() {
-                                            angular.element($elem).css('top', this.y + 'px');
-                                            $scope.direction.last = $scope.direction.current;
-                                            $scope.progress.rendered = this.y;
-                                            Utils.calculatePercent(this.y);
+                                            Utils.render(this.y);
                                         })
                                         .onComplete(function(){
-                                            $scope.tween = null;
-                                            $scope.direction.current = 0;
-                                            $scope.direction.last = 0;
+                                            data.tween = null;
+                                            data.direction.current = 0;
+                                            data.direction.last = 0;
                                         })
                                         .start();
                                 }
                             },
                             terminate: function() {
-                                if ($scope.tween) { 
-                                    $scope.tween.stop();
-                                    $scope.tween = null;
+                                if (data.tween) { 
+                                    data.tween.stop();
+                                    data.tween = null;
                                 }
                             }
                         },
@@ -69,6 +123,16 @@
                             } else if ($scope.scrollTotal < -(scrollableHeight)) {
                                 $scope.scrollTotal = -scrollableHeight;
                             }
+                        },
+                        checkLimit: function(delta) {
+                            var dif = $scope.scrollTotal - data.progress.last.y;
+                            //console.log(dif);
+                            var limit = 300;
+                            if (dif > limit || dif < -limit) {
+                                console.log('limiting');
+                                var limiter = dif - (limit*data.direction.current);
+                                $scope.scrollTotal -= (limiter/2);
+                            }
                         }
                     }
 
@@ -78,38 +142,116 @@
                      * This is also where the delta normalization happens.
                      */
                     var Handle = {
+                        /**
+                         * A mousewhel scroll.
+                         */
                         wheel: function(event) {
                             var e = event || window.event;
 
                             var o = e,
                                 d = o.detail, 
-                                w = o.wheelDeltaY || o.deltaY*-40 || (e.wheelDeltaY===undefined && e.wheelDelta) || e.detail || 0,
+                                w = o.wheelDeltaY || o.deltaY*-10 || (e.wheelDeltaY===undefined && e.wheelDelta) || e.detail || 0,
                                 n = 225, 
                                 n1 = n-1;
                         
                             // Normalize delta
-                            d = d ? w && (f = w/d) ? d/f : -d/1.35 : w/120;
+                            // d = d ? w && (f = w/d) ? d/f : -d/1.35 : w/120;
                             // Quadratic scale if |d| > 1
-                            d = d < 1 ? d < -1 ? (-Math.pow(d, 2) - n1) / n : d : (Math.pow(d, 2) + n1) / n;
+                            // d = d < 1 ? d < -1 ? (-Math.pow(d, 2) - n1) / n : d : (Math.pow(d, 2) + n1) / n;
                             // Delta *should* not be greater than 2 (before being multiplied)...
-                            var delta = (Math.min(Math.max(d / 2, -1), 1))*100;
+                            //var delta = (Math.min(Math.max(d / 2, -1), 1));
 
-                            $scope.direction.current = (delta > 0) ? 1 : -1;
+                            var delta = w;
+
+                            data.direction.current = (delta > 0) ? 1 : -1;
+
                             $scope.scrollTotal += delta;
 
+                            //Utils.checkLimit(delta);
                             Utils.checkRange();
-                            Utils.tween.set(delta);                        
+                            Utils.tween.set();                        
 
                             if (e.preventDefault) e.preventDefault();
                             if (e.stopPropagation) e.stopPropagation();
                             e.cancelBubble = true;  // IE events
                             e.returnValue = false;  // IE events
                             return false;
+                        },
+                        /**
+                         * Touchscreen events.
+                         */
+                        touch: {
+                            startY: 0,
+                            cur: 0,
+                            lastDelta: 0,
+                            start: function(e) {
+                                Handle.touch.startY = Handle.touch.cur = e.pageY;
+                                Utils.tween.terminate();
+                            },
+                            move: function(e) {
+                                var delta = (e.pageY - Handle.touch.cur) * 1.75;
+
+                                if (e.pageY !== 0) { Handle.touch.lastDelta = delta; }
+
+                                Handle.touch.cur = e.pageY;
+
+                                data.direction.current = (delta > 0) ? 1 : -1;
+                                $scope.scrollTotal += delta;
+                                data.progress.current.y = data.progress.last.y = $scope.scrollTotal;
+
+                                Utils.checkRange();
+                                Utils.render($scope.scrollTotal);
+
+                                if (e.preventDefault) e.preventDefault();
+                                if (e.stopPropagation) e.stopPropagation();
+                            },
+                            end: function(e) {
+                                $scope.scrollTotal = data.progress.rendered + (Handle.touch.lastDelta * 3);
+                                Utils.checkRange();
+                                Utils.tween.set({y:$scope.scrollTotal}, {y:data.progress.rendered});
+                            }
+                        },
+                        /**
+                         * Keyboard events.
+                         */
+                        key: function(e) {
+                            var down = 40;
+                            var up = 38
+                            var space = 32;
+
+                            if (e.keyCode === 40 || e.keyCode === 32) {
+                                data.direction.current = -1;
+                                var delta = e.keyCode === 32 ? -100 : -20;
+                            } else if (e.keyCode === 38 ) {
+                                data.direction.current = 1;
+                                var delta = 20;
+                            } else {    
+                                return;
+                            }
+
+                            $scope.scrollTotal += delta;
+                            data.progress.current.y = data.progress.last.y = $scope.scrollTotal;
+
+                            Utils.checkRange();
+
+                            if (e.keyCode === 32) {
+                                Utils.tween.set({y:$scope.scrollTotal}, {y:data.progress.rendered}, 500);
+                            } else {
+                                Utils.render($scope.scrollTotal);
+                            }
                         }
                     }
 
-                    $elem[0].onwheel = Handle.wheel;
-                    $elem[0].onmousewheel = Handle.wheel;
+                    transformPrefix = Utils.getPrefix(['transform', 'msTransform', 'MozTransform', 'WebkitTransform', 'OTransform']);
+
+                    el[0].onwheel = Handle.wheel;
+                    el[0].onmousewheel = Handle.wheel;
+
+                    el[0].addEventListener('touchstart', Handle.touch.start, false);
+                    el[0].addEventListener('touchmove', Handle.touch.move, false);
+                    el[0].addEventListener('touchend', Handle.touch.end, false);
+
+                    window.onkeydown = Handle.key;
 
                     raf.add('tween', 'everyFrame', function() {
                         TWEEN.update();
@@ -117,10 +259,11 @@
 
                     raf.add('masterScrollTween', 'everyFrame', function(){
                         if ($scope.paused) return;
-                        if (!$scope.tween) return;
+                        if (!data.tween) return;
 
-                        if (Math.ceil($scope.progress.current.y) !== Math.floor($scope.scrollTotal)) {                            
-                            $scope.progress.current.y += ($scope.scrollTotal - $scope.progress.current.y)/2;                            
+                        if (Math.ceil(data.progress.current.y) !== Math.floor($scope.scrollTotal)) {
+
+                            data.progress.current.y += ($scope.scrollTotal - data.progress.current.y)/2;                            
                         }
                     });
                     raf.start();
